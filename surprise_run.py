646b9f1fda2c685358cee79a8d4e1f485975c389
mark.ma <mark.ma@qingbao.cn>
2018-05-21 09:53:01 +0800

    surprise框架

--- surprise_run.py
+++ surprise_run.py
@@ -4,7 +4,7 @@
 @file: surprise_run.py 
 @time: 2018/05/17
 """
-from surprise import Dataset
+from surprise import Dataset, GridSearch
 from surprise import SVD
 from surprise.model_selection import cross_validate
 
@@ -16,3 +16,29 @@ algo = SVD()
 
 # Run 5-fold cross-validation and print results.
 cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+# 定义好需要优选的参数网格
+param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005],
+              'reg_all': [0.4, 0.6]}
+# 使用网格搜索交叉验证
+grid_search = GridSearch(SVD, param_grid, measures=['RMSE', 'FCP'])
+# 在数据集上找到最好的参数
+data = Dataset.load_builtin('ml-100k')
+data.split(n_folds=3)
+grid_search.evaluate(data)
+# 输出调优的参数组
+# 输出最好的RMSE结果
+print(grid_search.best_score['RMSE'])
+# >>> 0.96117566386
+
+# 输出对应最好的RMSE结果的参数
+print(grid_search.best_params['RMSE'])
+# >>> {'reg_all': 0.4, 'lr_all': 0.005, 'n_epochs': 10}
+
+# 最好的FCP得分
+print(grid_search.best_score['FCP'])
+# >>> 0.702279736531
+
+# 对应最高FCP得分的参数
+print(grid_search.best_params['FCP'])
+# >>> {'reg_all': 0.6, 'lr_all': 0.005, 'n_epochs': 10}
